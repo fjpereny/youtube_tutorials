684bfcbf9cf0a9ba0ed19d080879ca4f7b9e84bb
Frank Pereny <fjpereny@gmai.com>
2022-12-27 19:55:02 +0800

    Updated chat tutorial

--- Computer Vision/AI Gaming Health Bar Monitor/Part 5/chat_agent.py
+++ Computer Vision/AI Gaming Health Bar Monitor/Part 5/chat_agent.py
@@ -1,4 +1,5 @@
 
+
 import random
 import pyautogui
 import time
@@ -15,53 +16,57 @@ class ChatType:
 
 class ChatAgent:
     def __init__(self) -> None:
-        self.msg_queue = []
-        self.msg_delay = 3
         self.is_chatting = False
         self.cur_chat_type = None
 
-    def chat(self, msg, chat_type=ChatType.say, caps=False):
-        
+
+    def chat(self, msg, chat_type=ChatType.say, caps=False, lower=False):
         self.is_chatting = True
 
         if caps:
             msg = msg.upper()
+        if lower:
+            msg = msg.lower()
 
-        # Initialize chat by pressing the enter key
+        
+
+        # Initialize chat by the enter key
         pyautogui.press('enter')
-        rand_sleep_time = random.random() / 5.0 + 0.1
+        rand_sleep_time = random.random() / 3.0 + 0.1
         time.sleep(rand_sleep_time)
 
-        # Call correct chat type (say / guild / etc)
+        # Call the correct chat type (say / guild / party)
         if chat_type != self.cur_chat_type:
-            rand_type_time = random.randint(1, 5) * 0.005 * len(msg)
+            rand_type_time = random.random() / 4.0  + 0.1
             pyautogui.typewrite(chat_type, interval=rand_type_time)
-            rand_sleep_time = random.random() / 5.0 + 0.1
-            time.sleep(rand_sleep_time)
             self.cur_chat_type = chat_type
+        rand_sleep_time = random.random() / 3.0 + 0.1
+        time.sleep(rand_sleep_time)
 
-        # Type and send message
+
+        # Type our message and send it
         words = msg.split(' ')
-        for word in words:
+        for i in range(0, len(words)):
+            word = words[i]
             rand_type_time = random.randint(4, 7) * 0.003 * len(word)
             pyautogui.typewrite(word, interval=rand_type_time)
-            if word != words[-1]:
+            if i != len(words) - 1:
+                time.sleep(0.005)
                 pyautogui.press('space')
+
+        rand_sleep_time = random.random() / 2.0 + 0.1
+
         pyautogui.press('enter')
+
 
         self.is_chatting = False
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     chat_agent = ChatAgent()
-    while True:
-        chat_agent.chat("Hello, World!")
-        chat_agent.chat("This is still a 'say' chat...")
-        chat_agent.chat("yo!", chat_type=ChatType.yell, caps=True)
-        time.sleep(5)
-
-
-
+    chat_agent.chat("Hello, world!")
+    chat_agent.chat("Testing, 1 2 3!")
+    chat_agent.chat("This is a very long string.... let's see how this works!")
+    chat_agent.chat("Hello, world!", chat_type=ChatType.party, lower=True)
 
 
