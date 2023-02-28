c05c156bdc5995005ea608bccfa6dafc7c722b8a
Frank Pereny <fjpereny@gmai.com>
2023-02-28 08:29:32 +0800

    Updates

--- Computer Vision/Webcam Capture/capture.py
+++ Computer Vision/Webcam Capture/capture.py
@@ -8,24 +8,26 @@ cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise IOError("Cannot open video capture")
 
+
 while True:
     ret, frame = cap.read()
-    frame = cv2.resize(frame, dsize=(0, 0), fx=.5, fy=.5)
+
+    frame = cv2.resize(frame, dsize=(0, 0), fx=0.5, fy=0.5)
 
     class_names = [
-        'fresh apple', 
-        'fresh banana',  
-        'fresh orange', 
-        'fresh tomato', 
-        ]
+        'apple',
+        'banana',
+        'tomato',
+        'orange',
+    ]
 
-    # choice, score = img_predict(model, class_names, frame)
+    # choice, score = img_predicut(model, class_names, frame)
     choice = class_names[0]
     score = 0.984
 
     cv2.putText(
-        frame, 
-        choice, 
+        frame,
+        choice,
         (35, 35),
         cv2.FONT_HERSHEY_DUPLEX,
         0.8,
@@ -33,10 +35,10 @@ while True:
         1,
         cv2.LINE_AA,
     )
-    conf = np.max(score)
+
     cv2.putText(
-        frame, 
-        f"{conf * 100:.2f}%", 
+        frame,
+        str(f"{score * 100:.2f}%"),
         (35, 70),
         cv2.FONT_HERSHEY_DUPLEX,
         0.8,
@@ -44,12 +46,12 @@ while True:
         1,
         cv2.LINE_AA,
     )
-    cv2.imshow("Capture", frame)
 
-    c = cv2.waitKey(1)
-    if c == 27: # Esc key break
+    cv2.imshow("Webcam", frame)
+    key = cv2.waitKey(10)
+    if key == 27: # Esc key break
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
+    
