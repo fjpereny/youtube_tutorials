7013e28f309952136e57cf0a7dd978c145bf7e97
Frank Pereny <fjpereny@gmai.com>
2023-03-17 12:27:14 +0800

    update all

---
+++ Comp Vision Bot/settings.py
@@ -0,0 +1,9 @@
+
+# Computer Vision Settings
+# Set computer vision screen area
+COMP_VIZ_TOP_LEFT = (0, 0)
+COMP_VIZ_BOTTOM_RIGHT = (1920, 1080)
+
+# Turn on/off computer vision window
+ENABLE_PREVIEW = False
+
